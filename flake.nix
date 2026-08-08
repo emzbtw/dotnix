33cc4c208cc7ea8a3eb508aa32e3647dab9afde0
emz <307707176+emzbtw@users.noreply.github.com>
2026-08-08 17:12:12 +0100

    modules/secrets: add sops-nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +17,7 @@
     self,
     nixpkgs,
     nix-flatpak,
+    sops-nix,
     spicetify-nix,
     ...
   } @ inputs: {
@@ -21,6 +26,7 @@
       modules = [
         ./configuration.nix
         nix-flatpak.nixosModules.nix-flatpak
+        sops-nix.nixosModules.sops
         spicetify-nix.nixosModules.default
       ];
     };
---
+++ modules/secrets.nix
@@ -0,0 +1,14 @@
+{config, ...}: {
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  sops.age.generateKey = true;
+  sops.defaultSopsFile = ../secrets/secrets.yaml;
+
+  sops.secrets."sonarr/api_key" = {};
+  sops.secrets."sonarr/password" = {};
+  sops.secrets."radarr/api_key" = {};
+  sops.secrets."radarr/password" = {};
+  sops.secrets."prowlarr/api_key" = {};
+  sops.secrets."prowlarr/password" = {};
+  sops.secrets."sabnzbd/api_key" = {};
+  sops.secrets."jellyfin/admin_password" = {};
+}
