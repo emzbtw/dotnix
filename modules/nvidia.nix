591e82c9a2bb1e32dd2b556be380ae715e71a0f7
emz <307707176+emzbtw@users.noreply.github.com>
2026-08-16 17:05:10 +0100

    nvidia: added cache hit for CUDA

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,9 +1,13 @@
 {config, ...}: {
-  # NVIDIA
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia.open = false;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.powerManagement.enable = true;
+
+  nix.settings = {
+    substituters = ["https://cache.nixos-cuda.org"];
+    trusted-public-keys = ["cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="];
+  };
 }
