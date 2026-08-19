401ba2d492a6c2259ca2233250f871f65c333487
emz <307707176+emzbtw@users.noreply.github.com>
2026-08-19 21:49:06 +0100

    modules/nixflix: updated sabnzbd settings

--- modules/nixflix.nix
+++ modules/nixflix.nix
@@ -70,6 +70,13 @@
         misc = {
           api_key = {_secret = config.sops.secrets."sabnzbd/api_key".path;};
           nzb_key = {_secret = config.sops.secrets."sabnzbd/nzb_key".path;};
+          bandwidth_max = "100MB/s";
+          bandwidth_perc = 80;
+          cache_limit = "1G";
+          download_dir = "/var/lib/sabnzbd/incomplete";
+          download_free = "25G";
+          unwanted_extensions = "ade, adp, app, application, appref-ms, asp, aspx, asx, bas, bat, bgi, cab, cer, chm, cmd, cnt, com, cpl, crt, csh, der, diagcab, exe, fxp, gadget, grp, hlp, hpj, hta, htc, inf, ins, iso, isp, its, jar, jnlp, js, jse, ksh, lnk, mad, maf, mag, mam, maq, mar, mas, mat, mau, mav, maw, mcf, mda, mdb, mde, mdt, mdw, mdz, msc, msh, msh1, msh2, mshxml, msh1xml, msh2xml, msi, msp, mst, msu, ops, osd, pcd, pif, pl, plg, prf, prg, printerexport, ps1, ps1xml, ps2, ps2xml, psc1, psc2, psd1, psdm1, pst, py, pyc, pyo, pyw, pyz, pyzw, reg, scf, scr, sct, shb, shs, sln, theme, tmp, url, vb, vbe, vbp, vbs, vcxproj, vhd, vhdx, vsmacros, vsw, webpnp, website, ws, wsc, wsf, wsh, xbap, xll, xnk";
+          action_on_unwanted_extensions = 2;
         };
         servers = [
           {
