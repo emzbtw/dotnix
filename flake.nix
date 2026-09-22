58566724b397f83a532f13879e14488825d935ad
emz <307707176+emzbtw@users.noreply.github.com>
2026-09-22 22:37:52 +0100

    flake: name the flake dotnix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My NixOS flake";
+  description = "dotnix: single-machine NixOS flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
